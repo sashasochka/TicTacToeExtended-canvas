308e4f3c33177494469a230f673258469944453d
Oleksandr <sasha.sochka@gmail.com>
2013-08-25 13:09:17 +0300

    Massive refactoring and restructure

--- js/game_client.js
+++ js/game_client.js
@@ -2,10 +2,10 @@
 
 console.assert(typeof TicTacToeGame !== 'undefined');
 
-var game = new TicTacToeGame();
 
-var displaySettings = new function () {
-  this.container = 'TicTacToeCanvas';
+var Client = function (container, width, height) {
+  this.container = container;
+  this.gameEngine = new TicTacToeGame();
 
   // colors
   this.backgroundColor = 'orange';
@@ -16,8 +16,8 @@ var displaySettings = new function () {
   this.innerSquareGridLineColor = 'gray';
 
   // dimensions
-  this.width = 800;
-  this.height = this.width;
+  this.width = width;
+  this.height = height || this.width;
   this.crossStrokeWidth = this.width / 160;
   this.circleStrokeWidth = this.width / 160;
   this.cellBorderWidth = Math.min(this.width, this.height) / 140;
@@ -34,152 +34,170 @@ var displaySettings = new function () {
     y: this.cellBorderWidth / 2 + this.freeSpaceBetweenCells.y / 2 + 3
   };
   this.cellsDim = {
-    width:  this.width / game.size -
+    width:  this.width / this.gameEngine.size -
       this.cellBorderWidth - this.freeSpaceBetweenCells.x / 2,
-    height:  this.height / game.size -
+    height:  this.height / this.gameEngine.size -
       this.cellBorderWidth - this.freeSpaceBetweenCells.y / 2
   };
   this.cellsCoordDiff = {
-    x: (this.width - this.freeSpaceBetweenCells.x) / game.size,
-    y: (this.height - this.freeSpaceBetweenCells.y ) / game.size
+    x: (this.width - this.freeSpaceBetweenCells.x) / this.gameEngine.size,
+    y: (this.height - this.freeSpaceBetweenCells.y ) / this.gameEngine.size
   };
-}();
-
-var stage = new Kinetic.Stage({
-    container: displaySettings.container,
-    width: displaySettings.width,
-    height: displaySettings.height
-  }),
-  layer = new Kinetic.Layer();
 
 
-var SmallCell = function(coord) {
+  // KineticJS elements
+  this.stage = new Kinetic.Stage({
+      container: this.container,
+      width: this.width,
+      height: this.height
+    });
+  this.layer = new Kinetic.Layer();
+};
+
+// constants
+Client.horizontal = 0;
+Client.vertical = 1;
+
+Client.Cell = function (client, coord) {
   var rectSettings = {
-    x: displaySettings.firstCellCoord.x + coord.x * displaySettings.cellsCoordDiff.x,
-    y: displaySettings.firstCellCoord.y + coord.y * displaySettings.cellsCoordDiff.y,
-    width: displaySettings.cellsDim.width,
-    height: displaySettings.cellsDim.height,
-    fill: displaySettings.cellColor,
-    stroke: displaySettings.cellBorderColor,
-    strokeWidth: displaySettings.cellBorderWidth
+    x: client.firstCellCoord.x + coord.x * client.cellsCoordDiff.x,
+    y: client.firstCellCoord.y + coord.y * client.cellsCoordDiff.y,
+    width: client.cellsDim.width,
+    height: client.cellsDim.height,
+    fill: client.cellColor,
+    stroke: client.cellBorderColor,
+    strokeWidth: client.cellBorderWidth
   };
   Kinetic.Rect.call(this, rectSettings);
   this.on('click', function () {
-    var player = game.currentPlayer;
-    if (game.makeTurn(coord)) {
+    var player = client.gameEngine.currentPlayer;
+    if (client.gameEngine.makeTurn(coord)) {
       if (player === 1) {
         var crossMainDiagonalLine = new Kinetic.Line({
           points: [
-            rectSettings.x + displaySettings.crossDiagonalPadding.x,
-            rectSettings.y + displaySettings.crossDiagonalPadding.x,
-            rectSettings.x + rectSettings.width - displaySettings.crossDiagonalPadding.x,
-            rectSettings.y + rectSettings.height - displaySettings.crossDiagonalPadding.y
+            rectSettings.x + client.crossDiagonalPadding.x,
+            rectSettings.y + client.crossDiagonalPadding.x,
+            rectSettings.x + rectSettings.width - client.crossDiagonalPadding.x,
+            rectSettings.y + rectSettings.height - client.crossDiagonalPadding.y
           ],
-          stroke: displaySettings[player === 1 ? 'firstPlayerColor' : 'secondPlayerColor'],
-          strokeWidth: displaySettings.crossStrokeWidth,
+          stroke: client[player === 1 ? 'firstPlayerColor' : 'secondPlayerColor'],
+          strokeWidth: client.crossStrokeWidth,
           lineCap: 'round',
           lineJoin: 'round'
         });
         var crossAdditionalDiagonalLine = new Kinetic.Line({
           points: [
-            rectSettings.x + displaySettings.crossDiagonalPadding.x,
-            rectSettings.y + rectSettings.height - displaySettings.crossDiagonalPadding.y,
-            rectSettings.x + rectSettings.width - displaySettings.crossDiagonalPadding.x,
-            rectSettings.y + displaySettings.crossDiagonalPadding.x
+            rectSettings.x + client.crossDiagonalPadding.x,
+            rectSettings.y + rectSettings.height - client.crossDiagonalPadding.y,
+            rectSettings.x + rectSettings.width - client.crossDiagonalPadding.x,
+            rectSettings.y + client.crossDiagonalPadding.x
           ],
-          stroke: displaySettings[player === 1 ? 'firstPlayerColor' : 'secondPlayerColor'],
-          strokeWidth: displaySettings.crossStrokeWidth,
+          stroke: client[player === 1 ? 'firstPlayerColor' : 'secondPlayerColor'],
+          strokeWidth: client.crossStrokeWidth,
           lineCap: 'round',
           lineJoin: 'round'
         });
-        layer.add(crossMainDiagonalLine);
-        layer.add(crossAdditionalDiagonalLine);
+        client.layer.add(crossMainDiagonalLine);
+        client.layer.add(crossAdditionalDiagonalLine);
       } else {
         var circle = new Kinetic.Circle({
           x: rectSettings.x + rectSettings.width / 2,
           y: rectSettings.y + rectSettings.height / 2,
           radius: rectSettings.width / 2 - rectSettings.width / 15,
-          stroke: displaySettings[player === 1 ? 'firstPlayerColor' : 'secondPlayerColor'],
+          stroke: client[player === 1 ? 'firstPlayerColor' : 'secondPlayerColor'],
           strokeWidth: 4
         });
-        layer.add(circle);
+        client.layer.add(circle);
       }
-      stage.draw();
+      client.stage.draw();
 
-      if (game.winner()) {
-        $('#notification').html('Winner: ' + game.winner());
+      if (client.gameEngine.winner()) {
+        $('#notification').html('Winner: ' + client.gameEngine.winner());
       } else {
-        $('#notification').html('Move of player: ' + game.currentPlayer);
+        $('#notification').html('Move of player: ' + client.gameEngine.currentPlayer);
       }
     } else {
-      $('#notification').html('Invalid move! Player: ' + game.currentPlayer);
+      $('#notification').html('Invalid move! Player: ' + client.gameEngine.currentPlayer);
     }
   });
 };
 
-SmallCell.prototype = Object.create(Kinetic.Rect.prototype);
+Client.Cell.prototype = Object.create(Kinetic.Rect.prototype);
 
+Client.prototype.addBackground = function () {
+  this.layer.add(new Kinetic.Rect({
+    x: 0,
+    y: 0,
+    width: this.width,
+    height: this.height,
+    fill: this.backgroundColor,
+    stroke: this.innerSquareGridLineColor,
+    strokeWidth: 0
+  }));
+};
 
-layer.add(new Kinetic.Rect({
-  x: 0,
-  y: 0,
-  width: displaySettings.width,
-  height: displaySettings.height,
-  fill: displaySettings.backgroundColor,
-  stroke: displaySettings.innerSquareGridLineColor,
-  strokeWidth: 0
-}));
-
-// add the shape to the layer
-for (var y = 0; y < game.size; ++y) {
-  for (var x = 0; x < game.size; ++x) {
-    layer.add(new SmallCell({y: y,  x : x}));
+Client.prototype.addCells = function () {
+  // add the shape to the layer
+  for (var y = 0; y < this.gameEngine.size; ++y) {
+    for (var x = 0; x < this.gameEngine.size; ++x) {
+      this.layer.add(new Client.Cell(this,  {y: y,  x : x}));
+    }
   }
-}
+};
 
-var horizontal = 0, vertical = 1;
-var drawInnerSquareGridLine = function (index, direction) {
+Client.prototype.addSquareGridLines = function () {
+  for (var index = 0; index <= this.gameEngine.baseSize; ++index) {
+    this._drawInnerGridLine(this.gameEngine.baseSize * index, Client.horizontal);
+    this._drawInnerGridLine(this.gameEngine.baseSize * index, Client.vertical);
+  }
+};
+
+Client.prototype._drawInnerGridLine = function (index, direction) {
   var point1, point2;
-  if (direction === horizontal) {
+  if (direction === Client.horizontal) {
     point1 = {
       x: 0,
-      y: displaySettings.firstCellCoord.y + game.baseSize * index * displaySettings.cellsCoordDiff.y -
-        displaySettings.freeSpaceBetweenCells.y / 2 - displaySettings.cellBorderWidth / 2 + 1
+      y: this._innerGridLineCoordFormula(index, 'y')
     };
     point2 = {
-      x: displaySettings.width,
-      y: displaySettings.firstCellCoord.y + game.baseSize * index * displaySettings.cellsCoordDiff.y -
-        displaySettings.freeSpaceBetweenCells.y / 2 - displaySettings.cellBorderWidth / 2 + 1
+      x: this.width,
+      y: this._innerGridLineCoordFormula(index, 'y')
     };
   } else {
     point1 = {
-      x: displaySettings.firstCellCoord.x + game.baseSize * index * displaySettings.cellsCoordDiff.x -
-        displaySettings.freeSpaceBetweenCells.x / 2 - displaySettings.cellBorderWidth / 2 + 1,
+      x: this._innerGridLineCoordFormula(index, 'x'),
       y: 0
     };
     point2 = {
-      x: displaySettings.firstCellCoord.x + game.baseSize * index * displaySettings.cellsCoordDiff.x -
-        displaySettings.freeSpaceBetweenCells.x / 2 - displaySettings.cellBorderWidth / 2 + 1,
-      y: displaySettings.width
+      x: this._innerGridLineCoordFormula(index, 'x'),
+      y: this.width
     };
   }
-  layer.add(new Kinetic.Line({
-    points: [
+  this.layer.add(new Kinetic.Line({
+    points : [
       point1.x,
       point1.y,
       point2.x,
       point2.y
     ],
-    stroke: displaySettings.innerSquareGridLineColor,
-    strokeWidth: displaySettings.freeSpaceBetweenCells[horizontal ? 'y' : 'x'] + 1,
+    stroke: this.innerSquareGridLineColor,
+    strokeWidth: this.freeSpaceBetweenCells[Client.horizontal ? 'y' : 'x'] + 1,
     lineCap: 'round',
     lineJoin: 'round'
   }));
 };
 
-for (var index = 0; index <= game.baseSize; ++index) {
-  drawInnerSquareGridLine(index, horizontal);
-  drawInnerSquareGridLine(index, vertical);
-}
+Client.prototype._innerGridLineCoordFormula = function (index, dim) {
+  return this.firstCellCoord[dim] + index * this.cellsCoordDiff[dim] -
+    this.freeSpaceBetweenCells[dim] / 2 - this.cellBorderWidth / 2 + 1;
+};
 
-stage.add(layer);
+Client.prototype.display = function () {
+  this.stage.add(this.layer);
+};
+
+var client = new Client('TicTacToeCanvas', 800);
+client.addBackground();
+client.addCells();
+client.addSquareGridLines();
+client.display();
